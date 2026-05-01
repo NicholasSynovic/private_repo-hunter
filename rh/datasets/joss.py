0e157c1631150ffc9ac34cc466a96beb555cac52
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2026-05-01 07:42:49 -0500

    Add logging and update the issues class to reflect the schema

--- rh/datasets/joss.py
+++ rh/datasets/joss.py
@@ -1,16 +1,19 @@
 import json
-from typing import Any
+import logging
+import re
 
 from fastcore.all import AttrDict, obj2dict
 from pandas import DataFrame, Timestamp
-from progress.bar import Bar
 from pydantic import BaseModel
 
 from rh.apis.github import GitHub
 from rh.db import DB
+from rh.logger import get_logger, log_method
 
 GITHUB_AUTHOR: str = "openjournals"
 GITHUB_REPO: str = "joss-reviews"
+
+LOGGER: logging.Logger = get_logger(__name__)
 
 
 class Issue(BaseModel):
@@ -18,12 +21,17 @@ class Issue(BaseModel):
     created_at: int
     closed_at: int
     labels: str
+    accepted: bool = False
+    repository_url: str = ""
+    paper_url: str = ""
     raw_json: str
 
+    @log_method
     @staticmethod
     def convert_issue_id(issue_id) -> int:
         return int(issue_id)
 
+    @log_method
     @staticmethod
     def convert_datetime(dt: str) -> int:
         try:
@@ -31,22 +39,51 @@ class Issue(BaseModel):
         except TypeError:
             return -1
 
+    @log_method
     @staticmethod
     def convert_labels(labels: list[AttrDict]) -> str:
         data: list[str] = []
+
         label: AttrDict
         for label in labels:
             label_dict: dict = obj2dict(label)
             data.append(label_dict["name"])
+
         return json.dumps(obj=sorted(data))
 
+    @log_method
     @staticmethod
     def convert_raw_json(raw_json: dict) -> str:
         return json.dumps(obj=obj2dict(raw_json), indent=4)
 
+    @log_method
+    def extract_accepted_label(self) -> None:
+        self.accepted = '"accepted"' in self.labels
+
+    @log_method
+    def extract_repository_url(self, body: str) -> None:
+        split_body: list[str] = body.splitlines()
+        match = re.search(r'href="([^"]*)"', split_body[1])
+        if match:
+            self.repository_url = match.group(1)
+
+    @log_method
+    def extract_paper_url(self, body: str) -> None:
+        if self.accepted is False:
+            return None
+
+        pattern = re.compile(r"\]\((https?://joss\.theoj\.org/papers/[^)]+)\)$")
+
+        split_body: list[str] = body.splitlines()
+        line: str
+        for line in split_body[8:12]:
+            match = pattern.search(line)
+            if match:
+                self.paper_url = match.group(1)
+
 
 def extract() -> list[dict]:
-    print(f"Extracting {GITHUB_AUTHOR}/{GITHUB_REPO} issues...")
+    LOGGER.info("Extracting %s/%s issues...", GITHUB_AUTHOR, GITHUB_REPO)
     gh: GitHub = GitHub(owner=GITHUB_AUTHOR, repo=GITHUB_REPO)
     return gh.get_all_issues()
 
@@ -54,21 +91,31 @@ def extract() -> list[dict]:
 def transform(issues: list[dict]) -> DataFrame:
     data: list[Issue] = []
 
-    with Bar(
-        f"Transforming {GITHUB_AUTHOR}/{GITHUB_REPO} issues... ", max=len(issues)
-    ) as bar:
-        issue: dict[str, Any]
-        for issue in issues:
-            datum: Issue = Issue(
-                issue_id=Issue.convert_issue_id(issue["number"]),
-                created_at=Issue.convert_datetime(issue["created_at"]),
-                closed_at=Issue.convert_datetime(issue["closed_at"]),
-                labels=Issue.convert_labels(issue["labels"]),
-                raw_json=Issue.convert_raw_json(raw_json=issue),
-            )
+    LOGGER.info(
+        "Transforming %s/%s issues... count=%s",
+        GITHUB_AUTHOR,
+        GITHUB_REPO,
+        len(issues),
+    )
+    for issue in issues:
+        datum: Issue = Issue(
+            issue_id=Issue.convert_issue_id(issue["number"]),
+            created_at=Issue.convert_datetime(issue["created_at"]),
+            closed_at=Issue.convert_datetime(issue["closed_at"]),
+            labels=Issue.convert_labels(issue["labels"]),
+            raw_json=Issue.convert_raw_json(raw_json=issue),
+        )
+        datum.extract_accepted_label()
+        datum.extract_repository_url(body=issue["body"])
+        datum.extract_paper_url(body=issue["body"])
 
-            data.append(datum)
-            bar.next()
+        data.append(datum)
+
+    LOGGER.info(
+        "Finished transforming %s/%s issues.",
+        GITHUB_AUTHOR,
+        GITHUB_REPO,
+    )
 
     return DataFrame(data=[issue.model_dump() for issue in data])
 
